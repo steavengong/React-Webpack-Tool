666466a97f4373b9eadb7df6c550a0d7939fecfe
steavengong <842228597@qq.com>
2016-09-02 09:58:24 +0800

    mbp-v4

---
+++ src/app/common/config/index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Administrator on 2016/8/24.
+ */
+const Config = require('../../../common/config/index');
+
+Config.debug = true;
+let shareImage = require('../../assets/logo.png');
+
+config.requestAction = 'http://erpuat.mengbp.com:8090/wine-rest/cgi';
+config.requestActionDebug = 'http://erpuat.mengbp.com:8090/wine-rest/cgi';
+//Config.requestLocalAction = 'http://192.168.100.164:8081/wine-weixin-rest/cgi';
+
+Config.requestWXAction = 'http://erpuat.mengbp.com:8090/wine-weixin-rest/cgi';
+Config.requestWXActionDebug = 'http://erpuat.mengbp.com:8090/wine-weixin-rest/cgi';
+//Config.requestLocalWXAction = 'http://192.168.100.164:8081/wine-weixin-rest/cgi';
+
+Config.appId = 'wx5bb398c959489ae4';
+Config.appIdDebug = 'wx03fc01e909d9a654';
+
+Config.cmds = {
+  topicDetail:'smart/topic/detail',
+  serveDetail:'smart/services/getServicesDetails',
+  serviceEvaluate:'smart/serviceEvaluate/getPage',
+  wxJSSign:'christ/weixin/setSign',
+  detailDetail:'smart/topic/detail'
+}
+
+
+Config.urlParams = {
+    detailId:'detailId'
+}
+
+
+Config.shareObject = {
+    title:"萌宝派",
+    desc:"萌宝派",
+    link:window.location.href.split("#")[0].split("?")[0],
+    imgUrl:shareImage,
+}
+
+module.exports = Config;
---
+++ src/app/pages/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016/9/2.
+ */
+const React = require('react');
+require('normalize.css');
+require('./index.css');
+const ServePage = React.createClass({
+  render:function(){
+    return (
+      <div className="serve_page">
+
+      </div>
+    )
+  }
+});
+module.exports = ServePage;
