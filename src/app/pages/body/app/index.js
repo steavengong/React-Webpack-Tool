894e56001d73a0ddd250d5b4cb4753e84eaa20e0
steavengong <842228597@qq.com>
2016-09-07 17:30:32 +0800

    web-home

---
+++ src/app/pages/body/app/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2016/9/7.
+ */
+const React = require('react');
+import {Grid,Row,Col} from 'react-bootstrap'
+let mainBg = require('../../../assets/mbp_main_bg.png');
+let appModel = require('../../../assets/mpb_app_model.png');
+require('./index.css');
+const AppBody = React.createClass({
+  render:function(){
+    return (
+      <Grid>
+        <Row className="app_body_bg_row">
+          <Col lg={12} md={12} sm={12} xs={12} className="app_body_bg_col">
+            <img  src={mainBg} className="img_auto img_main_bg"/>
+            <div className="main_bg_left"></div>
+            <div className="main_bg_right"></div>
+            <div className="main_bg_border"></div>
+            <img src={appModel} className="img_app_model"/>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+})
+module.exports = AppBody;
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -6,11 +6,13 @@ require('bootstrap/dist/css/bootstrap.min.css');
 require('../styles/base.css');
 require('./index.css');
 const LogoHead = require('./head/logo/index');
+const AppBody = require('./body/app/index');
 const IndexPage = React.createClass({
   render:function(){
     return (
       <div className="home_page">
         <LogoHead/>
+        <AppBody/>
       </div>
     )
   }
