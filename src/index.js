0ae4fea541a5f90aa8a8627fd141da14d57f348e
steavengong <842228597@qq.com>
2016-09-07 14:17:10 +0800

    h5 wx

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const React = require('react');
 const ReactDom = require('react-dom');
 const App = require('./app/pages/index');
 
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?256d9ce857de599398c83beebbd4113d";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
 ReactDom.render(
   <App></App>,
   document.getElementById('app')
