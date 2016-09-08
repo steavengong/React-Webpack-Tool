3aa818e77d0b60cc189737068425d270ef2dd1da
steavengong <842228597@qq.com>
2016-09-08 10:25:15 +0800

    web-home

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -2,17 +2,12 @@
  * Created by Administrator on 2016/9/2.
  */
 const React = require('react');
-require('bootstrap/dist/css/bootstrap.min.css');
 require('../styles/base.css');
 require('./index.css');
-const LogoHead = require('./head/logo/index');
-const AppBody = require('./body/app/index');
 const IndexPage = React.createClass({
   render:function(){
     return (
       <div className="home_page">
-        <LogoHead/>
-        <AppBody/>
       </div>
     )
   }
