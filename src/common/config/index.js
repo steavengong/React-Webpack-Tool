873950ba07fe80b0e6cad88c533d125b8b61b456
steavengong <842228597@qq.com>
2016-09-13 15:32:12 +0800

    mbp v4 server

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -8,52 +8,52 @@ Config.requestAction = '';
 Config.requestActionDebug = '';
 Config.requestLocalAction = '';
 Config.getRequestAction = function(){
-    if(this.debug){
-        if(this.requestLocalAction){
-            return this.requestLocalAction;
-        }
-        return this.requestActionDebug;
+  if(this.debug){
+    if(this.requestLocalAction){
+      return this.requestLocalAction;
     }
-    return this.requestAction;
+    return this.requestActionDebug;
+  }
+  return this.requestAction;
 }
 Config.requestWXAction = '';
 Config.requestWXActionDebug = '';
 Config.requestLocalWXAction = '';
 Config.getRequestWXAction = function(){
-    if(this.debug){
-        if(this.requestLocalWXAction){
-            return this.requestLocalWXAction;
-        }
-        return this.requestWXActionDebug;
+  if(this.debug){
+    if(this.requestLocalWXAction){
+      return this.requestLocalWXAction;
     }
-    return this.requestWXAction;
+    return this.requestWXActionDebug;
+  }
+  return this.requestWXAction;
 }
 Config.upLoadAction = '';
 Config.upLoadActionDebug = '';
 Config.upLoadLocalAction = '';
 Config.getUpLoadAction = function(){
-    if(this.debug){
-        if(this.upLoadLocalAction){
-            return this.upLoadLocalAction;
-        }
-        return this.upLoadActionDebug;
+  if(this.debug){
+    if(this.upLoadLocalAction){
+      return this.upLoadLocalAction;
     }
-    return this.upLoadAction;
+    return this.upLoadActionDebug;
+  }
+  return this.upLoadAction;
 }
 
 Config.appId = '';
 Config.appIdDebug = '';
 Config.getAppId = function(){
-    if(this.debug){
-        return this.appIdDebug;
-    }
-    return this.appId;
+  if(this.debug){
+    return this.appIdDebug;
+  }
+  return this.appId;
 }
 
 Config.regs = {
-    image: /image\/\w+/,
-    phone: /^(13\d{9})|(147\d{8})|(15[02356789]\d{8})|(17[08]\d{8})|(18[012356789]\d{8})$/,
-    json: /{("\w+":"?\w+"?,?)+}/g,
+  image: /image\/\w+/,
+  phone: /^(13\d{9})|(147\d{8})|(15[02356789]\d{8})|(17[08]\d{8})|(18[012356789]\d{8})$/,
+  json: /{("\w+":"?\w+"?,?)+}/g,
   special: /[@#]([\u4E00-\u9FFF\w\-\+\*/\$\^\(\)\[\]\{\}\?\\\.\|!@#%&=~:：；;'"‘’“”￥……——。，（）｛｝【】《》<>？])+#?/g
 }
 
