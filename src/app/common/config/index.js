1c5c1643c941c0c6f6c295c3270d1e15ccc4970a
steavengong <842228597@qq.com>
2016-09-13 08:48:13 +0800

    mbp v4 server

--- src/app/common/config/index.js
+++ src/app/common/config/index.js
@@ -3,13 +3,13 @@
  */
 const Config = require('../../../common/config/index');
 
-Config.debug = true;
+Config.debug = false;
 let shareImage = require('../../assets/logo.png');
 
-Config.requestAction = 'http://erpuat.mengbp.com:8090/wine-rest/cgi';
+Config.requestAction = 'http://mengbaopai.smart-kids.com:84/smart-rest/cgi';
 Config.requestActionDebug = 'http://erpuat.mengbp.com:8090/wine-rest/cgi';
 
-Config.requestWXAction = 'http://erpuat.mengbp.com:8090/wine-weixin-rest/cgi';
+Config.requestWXAction = 'http://mengbaopai.smart-kids.com:84/wine-weixin-rest/cgi';
 Config.requestWXActionDebug = 'http://erpuat.mengbp.com:8090/wine-weixin-rest/cgi';
 
 Config.appId = 'wx5bb398c959489ae4';
