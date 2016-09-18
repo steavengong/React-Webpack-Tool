27a3205e22b7e24d5dc11b9b7e1dfc134361fa6e
steavengong <842228597@qq.com>
2016-09-18 11:27:35 +0800

    web

--- src/app/common/config/index.js
+++ src/app/common/config/index.js
@@ -5,25 +5,18 @@ const Config = require('../../../common/config/index');
 
 Config.debug = false;
 
-Config.bgImages = [
-  require('../../assets/section_part_1.jpg')
-]
-Config.textImages = []
-Config.phoneImages = [
-  require('../../assets/mpb_app_model.png')
-]
 Config.qrCode = require('../../assets/mbp_qr_code.png');
 Config.sectionParts = [
-  {bgImage: Config.bgImages[0],textImage:'',phoneImage:Config.phoneImages[0],qrCode:Config.qrCode}
+  {
+    bgImage: require('../../assets/section_part_1.jpg'),
+    textImage:'',
+    phoneImage:require('../../assets/mpb_app_model.png'),
+    qrCode:Config.qrCode}
 ]
 
-
-
-Config.logoLink = "http://www.baidu.com";
-Config.logoImage = require('../../assets/mbp_logo_head.png');
 Config.logoParts = {
-  logoLink : Config.logoLink,
-  logoImage: Config.logoImage
+  logoLink : "http://www.mengbp.com/web/PC/Portal/HomePage/",
+  logoImage: require('../../assets/mbp_logo_head.png')
 }
 
 
